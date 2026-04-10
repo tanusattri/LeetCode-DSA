4f75c7499e5c6620d078903c0a71fcaad857f1cd
Tanu Singh Attri <tanusattri06@gmail.com>
2026-04-10 09:57:33 +0530

    Time: 0 ms (100%), Space: 19.5 MB (20.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,16 +5,17 @@ class Solution:
         """
         p1= m-1
         p2= n-1
-        p= m+n-1
+        p3= m+n-1
         while p1>=0 and p2>=0:
-            if nums1[p1]>nums2[p2]:
-                nums1[p]= nums1[p1]
-                p1-=1
-            else:
-                nums1[p]= nums2[p2]
+            if nums1[p1]<nums2[p2]:
+                nums1[p3]= nums2[p2]
                 p2-=1
-            p-=1
+                p3-=1
+            else:
+                nums1[p3]= nums1[p1]
+                p1-=1
+                p3-=1
         while p2>=0:
-            nums1[p]= nums2[p2]
+            nums1[p3]= nums2[p2]
+            p3-=1
             p2-=1
-            p-=1
