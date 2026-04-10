eda1ab3768f827ab9849a7e9415952fcce7012c7
Tanu Singh Attri <tanusattri06@gmail.com>
2026-04-10 10:27:41 +0530

    Time: 3 ms (76.19%), Space: 26.6 MB (51.54%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,12 +4,11 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n= len(nums)
-        k%= n
-        #for right rotation
-        self.reverse(nums,0,n-1) #reverse the whole array
-        self.reverse(nums,0,k-1) #reverse the first k elements
-        self.reverse(nums,k,n-1) #reverse the rest of the array
-    def reverse(self,arr: List[int],start: int,end:int)-> None:
+        k%=n
+        self.reverse(nums,0,n-1)
+        self.reverse(nums,0,k-1)
+        self.reverse(nums,k,n-1)
+    def reverse(self, arr: List[int], start: int, end: int)->None:
         while start<end:
             arr[start], arr[end]= arr[end], arr[start]
             start+=1
