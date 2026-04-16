e6500f266a3652686721f444466063ccf37d6e65
Tanu Singh Attri <tanusattri06@gmail.com>
2026-04-16 10:02:42 +0530

    Time: 13 ms (50.46%), Space: 19.5 MB (52.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,8 +4,9 @@ class Solution:
         n= len(t)
         if m!=n:
             return False
-        countS, countT={}, {}
-        for i in range (m):
+        countS= {}
+        countT= {}
+        for i in range(m):
             countS[s[i]]= countS.get(s[i],0)+1
             countT[t[i]]= countT.get(t[i],0)+1
-        return countS==countT
+        return countS== countT
