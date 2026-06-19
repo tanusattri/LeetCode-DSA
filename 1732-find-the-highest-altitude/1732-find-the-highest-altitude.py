70ab13a4bbe64004c4e41edbc67cae38aece9275
Tanu Singh Attri <tanusattri06@gmail.com>
2026-06-19 14:52:39 +0530

    Time: 0 ms (100%), Space: 19.3 MB (15.4%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans= acc=0
+        for it in gain:
+            acc+= it
+            d= acc-ans
+            ans += d & ~(d >> 0x1F)
+        return ans 
